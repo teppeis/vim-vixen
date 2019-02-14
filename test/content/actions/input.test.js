48a040eeec9ce90a2ea5769c3c5728fe92bb08e0
teppeis <teppeis@gmail.com>
2019-02-14 23:07:12 +0900

    test: add a failed test case for power-assert demo

--- test/content/actions/input.test.js
+++ test/content/actions/input.test.js
@@ -1,19 +1,22 @@
 import actions from 'content/actions';
 import * as inputActions from 'content/actions/input';
+import assert from 'assert';
 
-describe("input actions", () => {
-  describe("keyPress", () => {
+describe('input actions', () => {
+  describe('keyPress', () => {
     it('create INPUT_KEY_PRESS action', () => {
       let action = inputActions.keyPress('a');
-      expect(action.type).to.equal(actions.INPUT_KEY_PRESS);
-      expect(action.key).to.equal('a');
+      assert(action.type === actions.INPUT_KEY_PRESS);
+      assert(action.key === 'a');
+      // Error!
+      assert(action.key === 'b');
     });
   });
 
-  describe("clearKeys", () => {
-    it('create INPUT_CLEAR_KEYSaction', () => {
+  describe('clearKeys', () => {
+    it('create INPUT_CLEAR_KEYS action', () => {
       let action = inputActions.clearKeys();
-      expect(action.type).to.equal(actions.INPUT_CLEAR_KEYS);
+      assert(action.type === actions.INPUT_CLEAR_KEYS);
     });
   });
 });
